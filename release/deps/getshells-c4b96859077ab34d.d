9702f0cef640a996212ee4f0a79677025323b0a7
izzy Meyer <izder456@disroot.org>
2026-05-26 22:47:56 -0500

    cleanup

--- release/deps/getshells-c4b96859077ab34d.d
+++
@@ -1,5 +0,0 @@
-/home/jjs/passwd-shell-benchmarks/getshells_rust/../release/deps/getshells-c4b96859077ab34d: src/main.rs
-
-/home/jjs/passwd-shell-benchmarks/getshells_rust/../release/deps/getshells-c4b96859077ab34d.d: src/main.rs
-
-src/main.rs:
