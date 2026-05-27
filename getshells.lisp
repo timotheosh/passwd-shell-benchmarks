aeaaae097f5fbd8b725ade92bfcd59a492ba14ea
izzy Meyer <izder456@disroot.org>
2026-05-26 23:27:44 -0500

    optimize and shorten lisp impl

--- getshells.lisp
+++ getshells.lisp
@@ -1,35 +1,11 @@
 #!sbcl --script
-(defun inc (val)
-  (declare
-   (optimize (speed 3) (debug 0) (safety 0)))
-  (if val
-      (1+ val)
-      1))
 
-(defun split-passwd (file)
-  (declare (type simple-string file)
-           (optimize (speed 3) (debug 0) (safety 0)))
-  (with-open-file (stream file :if-does-not-exist nil)
-    (when stream
-      (loop
-        for line = (read-line stream nil)
-        while line
-        for brk-mark = (position-if (lambda (char) (char= #\: char)) line :from-end t)
-        if brk-mark
-          collect (subseq line (1+ brk-mark))))))
-
-(defun occurrences (lst)
-  (declare (type simple-list lst)
-           (optimize (speed 3) (debug 0) (safety 0)))
-  (let ((table (make-hash-table :size (length lst) :test #'equal)))
-    (loop for shell in lst do
-      (setf (gethash shell table) (inc (gethash shell table))))
-    table))
-
-(defun main (&rest argv)
-  (declare (ignorable argv)
-           (optimize (speed 3) (debug 0) (safety 0)))
-  (loop for k being the hash-keys in (occurrences (split-passwd "passwd")) using (hash-value v)
-        do (format t "~a : ~a~%" k v)))
-
-(main)
+(let ((h (make-hash-table :test #'equal)))
+  (with-open-file (s "passwd")
+    (loop for l = (read-line s nil)
+          while l
+          for sh = (subseq l (1+ (position #\: l :from-end t)))
+          do (incf (gethash sh h 0))))
+  (maphash (lambda (k v)
+             (format t "~a : ~a~%" k v))
+           h))
