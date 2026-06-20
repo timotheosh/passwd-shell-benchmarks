9647dd01d165708b1353f0da214b726de8bfe874
Tim Hawes <trhawes@gmail.com>
2026-06-20 14:04:29 -0400

    Switch from Clojure to babashka

--- getshells.clj
+++ getshells.clj
@@ -1,15 +1,13 @@
-#!/usr/bin/env clojure
+#!/usr/bin/env bb
 
-(defn split-passwd [f]
-  ;; Open file inside with-open block so file streams don't leak
-  (with-open [r (clojure.java.io/reader f)]
-    ;; doall forces evaluation of the lazy map before the file stream closes
-    (doall (map #(last (clojure.string/split % #":"))
-                (line-seq r)))))
+(defn get-shell [line]
+  (let [idx (clojure.string/last-index-of line ":")]
+    (subs line (inc idx))))
 
 (defn -main [& _]
-  (doseq [[k v] (frequencies (split-passwd "passwd"))]
-    (println k ":" v)))
+  (with-open [r (clojure.java.io/reader "passwd")]
+    (doseq [[k v] (frequencies (map get-shell (line-seq r)))]
+      (println k ":" v))))
 
 ;; Check if arguments exist, otherwise fall back to empty args
 (apply -main *command-line-args*)
