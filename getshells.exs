18eb1c8dae19c885aeb347d7922bbe7472803d62
izzy Meyer <izder456@disroot.org>
2026-06-21 18:02:51 -0500

    faster elixir, crystal, and guile

--- getshells.exs
+++ getshells.exs
@@ -1,9 +1,125 @@
 #!/usr/bin/env elixir
 
-"passwd"
-|> File.stream!()
-|> Enum.map(&(String.split(String.trim(&1), ":") |> List.last()))
-|> Enum.frequencies()
-|> Enum.each(fn {shell, n} ->
-  IO.puts("#{String.pad_trailing(shell, 20)}: #{n}")
-end)
+defmodule GetShells do
+  @bufsize 1024 * 1024
+
+  def run do
+    {:ok, fd} = :file.open('passwd', [:read, :raw, :binary])
+
+    counts =
+      scan(fd, %{}, 0, false, [])
+
+    :file.close(fd)
+
+    Enum.each(counts, fn {shell, n} ->
+      IO.puts("#{shell} : #{n}")
+    end)
+  end
+
+  defp scan(fd, counts, colon_count, capturing, shell) do
+    case :file.read(fd, @bufsize) do
+      {:ok, data} ->
+        {counts, colon_count, capturing, shell} =
+          parse_chunk(
+            data,
+            counts,
+            colon_count,
+            capturing,
+            shell
+          )
+
+        scan(
+          fd,
+          counts,
+          colon_count,
+          capturing,
+          shell
+        )
+
+      :eof ->
+        counts
+    end
+  end
+
+  defp parse_chunk(
+         <<>>,
+         counts,
+         colon_count,
+         capturing,
+         shell
+       ) do
+    {counts, colon_count, capturing, shell}
+  end
+
+  defp parse_chunk(
+         <<?:, rest::binary>>,
+         counts,
+         colon_count,
+         _capturing,
+         shell
+       ) do
+    parse_chunk(
+      rest,
+      counts,
+      colon_count + 1,
+      colon_count + 1 == 6,
+      if(colon_count + 1 == 6, do: [], else: shell)
+    )
+  end
+
+  defp parse_chunk(
+         <<?\n, rest::binary>>,
+         counts,
+         _colon_count,
+         _capturing,
+         shell
+       ) do
+    counts =
+      if shell == [] do
+        counts
+      else
+        shell =
+          shell
+          |> Enum.reverse()
+          |> :erlang.list_to_binary()
+
+        Map.update(counts, shell, 1, &(&1 + 1))
+      end
+
+    parse_chunk(rest, counts, 0, false, [])
+  end
+
+  defp parse_chunk(
+         <<c, rest::binary>>,
+         counts,
+         colon_count,
+         true,
+         shell
+       ) do
+    parse_chunk(
+      rest,
+      counts,
+      colon_count,
+      true,
+      [c | shell]
+    )
+  end
+
+  defp parse_chunk(
+         <<_, rest::binary>>,
+         counts,
+         colon_count,
+         capturing,
+         shell
+       ) do
+    parse_chunk(
+      rest,
+      counts,
+      colon_count,
+      capturing,
+      shell
+    )
+  end
+end
+
+GetShells.run()
