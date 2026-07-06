1766e83c197a5580e28e60f60c0c581a94fc321e
Dick Johnson <einheit@mainphrame.com>
2026-07-06 14:37:43 -0700

    Substantial speedup for powershell program

--- src/getshells.ps1
+++ src/getshells.ps1
@@ -1,17 +1,6 @@
-#!/usr/bin/env pwsh 
-
-$shells = @{}
-
-foreach($line in $(get-content passwd)){
-   $shell = $line.split(':')[6]
-   if($shells.ContainsKey($shell)) {
-     $shells.Set_Item($shell, $shells.$shell+1) 
-  } else {
-    $shells.Add($shell, 1)
-  }
- }
-
-foreach($sh in $shells) {
-	echo $sh, $sh.shell;
-}
+#!/usr/bin/env pwsh
+[System.IO.File]::ReadLines("passwd") | 
+    ForEach-Object { $_.Split(':')[6] } | 
+    Group-Object -NoElement | 
+    ForEach-Object { "{0} : {1}" -f $_.Name, $_.Count }
 
